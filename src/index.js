f46f00678e010343c3b5a44adf996eca19abdcca
nmanuilova <nmanuilova@fix.ru>
2024-12-04 17:38:13 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -15,20 +15,21 @@ const build = (obj1, obj2) => {
     .flatMap((key) => {
       const val1 = obj1[key];
       const val2 = obj2[key];
+      let result = null;
 
       if (!_.has(obj1, key)) {
-        return { key, value: [val2], type: 'added' };
+        result = { key, value: [val2], type: 'added' };
+      } else if (!_.has(obj2, key)) {
+        result = { key, value: [val1], type: 'deleted' };
+      } else if (_.isObject(val1) && _.isObject(val2)) {
+        result = { key, value: build(val1, val2), type: 'nested' };
+      } else if (val1 === val2) {
+        result = { key, value: [val1], type: 'unchanged' };
+      } else {
+        result = { key, value: [val1, val2], type: 'changed' };
       }
-      if (!_.has(obj2, key)) {
-        return { key, value: [val1], type: 'deleted' };
-      }
-      if (_.isObject(val1) && _.isObject(val2)) {
-        return { key, value: build(val1, val2), type: 'nested' };
-      }
-      if (val1 === val2) {
-        return { key, value: [val1], type: 'unchanged' };
-      }
-      return { key, value: [val1, val2], type: 'changed' };
+
+      return result;
     });
 };
 
